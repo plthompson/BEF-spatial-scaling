c129c52e7b2ccce6fef670f4e7c1d407095d86c7
plthompson <patrickthompson9@gmail.com>
2017-01-16 15:48:05 -0800

    annotated script

--- scripts/BEF supplemental.R
+++ scripts/BEF supplemental.R
@@ -1,3 +1,7 @@
+#Code by Patrick Thompson
+#In collaboration with Andrew Gonzalez and Forest Isbell 
+#January 2017
+
 #packages####
 library(dplyr)
 library(broom)
@@ -6,18 +10,18 @@ library(ggExtra)
 library(cowplot)
 
 #Figure S1####
-spec_acc<-c(0,0.5,1)
+spec_acc<-c(0,0.5,1) #three rates of species accumulation
 results<-data.frame()
 for(s in spec_acc){
-  accum<-(1:10)^s
-  SR<-sapply(1:20,function(x){x*accum})
-  EF<-t(sapply(1:10,function(x){x*SR[1,]^0.26}))
+  accum<-(1:10)^s #species accumulation across 10 spatial scales
+  SR<-sapply(1:20,function(x){x*accum}) #simulate local richness gradients from 1:20 and correspond richness at 10 spatial scales
+  EF<-t(sapply(1:10,function(x){x*SR[1,]^0.26})) #calculate ecosystem functioning at all scales and levels of local richness
   
   results<-rbind(results,data.frame(SR=c(SR),EF=c(EF),Scale=1:10,spec_acc=s))
 }
 
 results<-results %>% 
-  mutate(slope=log(EF)/log(SR))
+  mutate(slope=log(EF)/log(SR)) #calculate BEF slope
 
 results$spec_acc<-paste("Sp. accum. rate = ",results$spec_acc,sep="")
 
@@ -45,23 +49,23 @@ plot_grid(S1a,S1b,ncol = 1)
 ggsave("./figures/Figure S1.pdf",width = 8,height=6)
 
 #Figure S2####
-Local_BEF_slope<-seq(-0.2,0.7,length=1000)
-function_5<-15^Local_BEF_slope
-function_4<-14^Local_BEF_slope
-function_change<-function_5-function_4
+Local_BEF_slope<-seq(-0.2,0.7,length=1000) #produce range of local b  
+function_5<-15^Local_BEF_slope #caculate the functioning of 15 species at each level of b
+function_4<-14^Local_BEF_slope #calculate the functioning of 14 species at each level of b
+function_change<-function_5-function_4 #calculate the difference
 inequality.df<-data.frame(BEF_slope=Local_BEF_slope,Function_change=function_change)
 
-Local_SR<-seq(1,40,length=1000)
-function_1<-Local_SR^0.26
-function_2<-(Local_SR+1)^0.26
-function_change<-function_2-function_1
+Local_SR<-seq(1,40,length=1000) #produce range of intial local richness
+function_1<-Local_SR^0.26 #calculate corresponding EF
+function_2<-(Local_SR+1)^0.26 #calculate EF with one additional species
+function_change<-function_2-function_1 #calculate the difference
 inequality.df2<-data.frame(Local_SR=Local_SR,Function_change=function_change)
 
 S2a<-ggplot(inequality.df,aes(x=BEF_slope,y=Function_change))+
   geom_line()+
   theme_bw()+
   removeGrid()+
-  xlab("Local BEF slope")+
+  xlab(expression(paste("Local ", italic("b"))))+
   ylab("Change in function")
   
 
@@ -77,17 +81,17 @@ ggsave("./figures/Figure S2.pdf",width = 8,height=4)
 
 #Figure S3####
 #variation in slopes
-sites<-50
-species<-40
+sites<-50 #number of local patches
+species<-40 #number of species
 
 slopes.df<-data.frame()
 for(i in 1:1000){
-  slopes<-rnorm(n = sites,mean = 0.26, sd = 0.11)
-  L_SR<-seq(1:species)
-  L_bmass<-sapply(L_SR,FUN = function(x){x^slopes})
+  slopes<-rnorm(n = sites,mean = 0.26, sd = 0.11) #draw local b from normal distribution
+  L_SR<-seq(1:species) #gradient of local richness
+  L_EF<-sapply(L_SR,FUN = function(x){x^slopes}) #calculate local EF for each level of richness with each local b
   
-  Reg_bmass<-apply(L_bmass,2,function(x){sapply(1:sites,FUN = function(y) {sum(x[1:y])})})
-  slopes<-apply(Reg_bmass,1,function(x){coef(lm(log(x)~log(c(1:species))))[2]})
+  Reg_EF<-apply(L_EF,2,function(x){sapply(1:sites,FUN = function(y) {sum(x[1:y])})}) #calculate EF at larger spatial scales
+  slopes<-apply(Reg_EF,1,function(x){coef(lm(log(x)~log(c(1:species))))[2]}) #calculate b at larger spatial scales
   
   slopes.df<-rbind(slopes.df,data.frame(Slopes=slopes,Scale=1:sites))
 }
@@ -101,20 +105,20 @@ S3a<-ggplot(slopes.summary,aes(x=Scale,y=Slopes))+
   geom_line()+
   theme_bw()+
   removeGrid()+
-  ylab("Biodiversity ecosystem function slope")
-
+  ylab(expression(italic("b")))+
+  xlab("Spatial scale")
 #variation in local richness
 slopes2.df<-data.frame()
 for(i in 1:1000){
-  slopes<-0.26
-  L_SR<-round(runif(sites,min = 1,max=20))
-  L_SR<-sapply(1:20, FUN = function(x){(L_SR+x)})
+  slopes<-0.26 #set local b
+  L_SR<-round(runif(sites,min = 1,max=20)) #generate random local SR
+  L_SR<-sapply(1:20, FUN = function(x){(L_SR+x)}) #diversity change
   
-  L_EF<-L_SR^0.26
+  L_EF<-L_SR^0.26 #calculate local functioning
   
-  Reg_EF<-apply(L_EF,2,function(x){sapply(1:sites,FUN = function(y) {sum(x[1:y])})})
-  Reg_S<-apply(L_SR,2,function(x){sapply(1:sites,FUN = function(y) {sum(x[1:y])})})
-  slopes<-sapply(1:sites,function(x){coef(lm(log(Reg_EF[x,])~log(Reg_S[x,])))[2]})
+  Reg_EF<-apply(L_EF,2,function(x){sapply(1:sites,FUN = function(y) {sum(x[1:y])})}) #calculate regional functioning
+  Reg_S<-apply(L_SR,2,function(x){sapply(1:sites,FUN = function(y) {sum(x[1:y])})}) #calculate regional SR
+  slopes<-sapply(1:sites,function(x){coef(lm(log(Reg_EF[x,])~log(Reg_S[x,])))[2]}) #calculate b at each spatial scale
   
   slopes2.df<-rbind(slopes2.df,data.frame(Slopes=slopes,Scale=1:sites))
 }
@@ -128,64 +132,8 @@ S3b<-ggplot(slopes.summary2,aes(x=Scale,y=slope_mean))+
   geom_line()+
   theme_bw()+
   removeGrid()+
-  ylab("Biodiversity ecosystem function slope")
+  ylab(expression(italic("b")))+
+  xlab("Spatial scale")
 
 plot_grid(S3a,S3b,ncol = 2,labels = c("a)","b)"))
 ggsave("./figures/Figure S3.pdf",width = 8,height=4)
-
-#Figure S4####
-all.results<-data.frame()
-for(S_total in c(10,40,400,4000)){
-  species_pool<-1:S_total
-  
-  species_comp<-matrix(NA,2000,10)
-  species_comp[1,]<-sapply(1:10,function(x){sample(species_pool,1,replace = T)})
-  S<-sapply(1:10,FUN = function(x){length(unique(species_comp[1,1:x]))})
-  
-  results<-data.frame(Scale=1:10,S=S,EF=1:10*(1^0.26),S_mean=1,beta_div="yes")
-  
-  for(i in 2:2000){
-    species_comp[i,sample(1:10,1,replace=F)]<-sample(species_pool,1,replace = F)
-    local_S<-apply(species_comp,2,function(x) {length(unique(x[!is.na(x)]))} )
-    local_EF<-local_S^0.26
-    reg_EF<-sapply(1:10,FUN = function(x){sum(local_EF[1:x])})
-    reg_S<-sapply(1:10,FUN= function(x){
-      region<-species_comp[,1:x]
-      length(unique(c(region[!is.na(region)])))})
-    results<-rbind(results, data.frame(Scale=1:10,S=reg_S,EF=reg_EF,S_mean=mean(local_S),beta_div="yes"))
-  }
-  
-  local_S<-1:round(max(results$S))
-  local_EF<-local_S^0.26
-  reg_EF<-local_EF*10
-  reg_S<-local_S
-  results<-rbind(results, data.frame(Scale=10,S=reg_S,EF=reg_EF,S_mean=reg_S,beta_div="no"))
-  results$species_pool<-S_total
-  
-  all.results<-rbind(all.results,results)
-}
-
-S4a<-ggplot(all.results,aes(x=S,y=EF,color=as.factor(Scale),group=interaction(Scale,beta_div),linetype=beta_div))+
-  geom_line()+
-  facet_wrap(~species_pool,nrow = 1,scale="free")+
-  scale_color_grey(start=0.8,end=0,name="Scale")+
-  xlab("Species richness")+
-  ylab("Ecosystem function")+
-  scale_linetype(name="Beta\ndiversity")+
-  theme_bw()+
-  removeGrid()
-
-S4b<-ggplot(all.results,aes(x=S,y=EF,color=as.factor(Scale),group=interaction(Scale,beta_div),linetype=beta_div))+
-  geom_line()+
-  facet_wrap(~species_pool,nrow = 1,scale="free")+
-  scale_color_grey(start=0.8,end=0,name="Scale")+
-  xlab("Species richness")+
-  ylab("Ecosystem function")+
-  scale_linetype(name="Beta\ndiversity")+
-  theme_bw()+
-  removeGrid()+
-  scale_x_log10()+
-  scale_y_log10()
-
-plot_grid(S4a,S4b,ncol = 1)
-ggsave("./figures/Figure S4.pdf",width = 10,height=7)
